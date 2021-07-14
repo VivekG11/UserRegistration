67d02350f45e35e0ca8ca4bccf2fd636c378c79f
VivekG11 <vivekvk2903@gmail.com>
2021-07-14 22:27:34 +0530

    [Validating First Name]

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration.......");
+            RegExp regExp = new RegExp();
+            regExp.ValidateFirstName();
+        }
+    }
+}
---
+++ UserRegistration/RegExp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace UserRegistration
+{
+    class RegExp
+    {
+       public static  string pattern = "^[A-Z][a-z]{2,}$";
+        public void ValidateFirstName()
+        {
+            Regex exp = new Regex(pattern);
+            Console.WriteLine("Enter first name :");
+            string firstName = Console.ReadLine();
+            bool result = Regex.IsMatch(firstName, pattern);
+           if(result)
+            {
+                Console.WriteLine("Valid");
+     
+            }
+           else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
